f31a9a508052752c6d3677593c616fc03ca107f7
debjohnson <debjohnson_11@yahoo.com>
2018-06-08 15:53:53 -0500

    Added gitignore file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171228003845) do
+ActiveRecord::Schema.define(version: 20180608203346) do
 
   create_table "goals", force: :cascade do |t|
     t.integer "user_id"
@@ -35,7 +35,6 @@ ActiveRecord::Schema.define(version: 20171228003845) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", null: false
