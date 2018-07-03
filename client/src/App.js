02a113b06445b185fa69428a1c8af986895fe7bd
debjohnson <debjohnson_11@yahoo.com>
2018-07-02 19:50:56 -0500

    Added react-router to app/started switch

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 import Home from './Home';
@@ -6,12 +7,14 @@ import Home from './Home';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Home />
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <Router>
+          <div className="App">
+            <Switch>
+              <Route exact path='/' component={Home}/>
+
+            </Switch>          
+          </div>
+        </Router>
     );
   }
 }
