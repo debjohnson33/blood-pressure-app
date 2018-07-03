9ef67270f51a49597df2f96d1a02aff6a4eb97d3
debjohnson <debjohnson_11@yahoo.com>
2018-07-02 19:25:34 -0500

    Added Home component to client

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Home from './Home';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to the Blood Pressure App</h1>
-        </header>
+        <Home />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ client/src/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home = (props) => {
+	return(
+		<div>
+			<header className="App-header">
+	          <h1 className="App-title">Welcome to the Blood Pressure App</h1>
+	        </header>
+	        <br />
+	        <br />
+	        <br />
+		</div>
+	)
+}
+
+export default Home;
