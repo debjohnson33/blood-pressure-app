097aadf8694e6c2dd30d27127b256535f967af53
debjohnson <debjohnson_11@yahoo.com>
2018-07-10 20:26:10 -0500

    Added jwt-decode to client & redirect to login

--- client/src/LoginComponent.js
+++ client/src/LoginComponent.js
@@ -2,28 +2,50 @@ import React, { Component } from 'react';
 
 class LoginComponent extends Component {
 
+	handleSubmit = event => {
+		event.preventDefault();
+		// console.log(this.inputNode1.value);
+		// console.log(this.inputNode2.value);
+
+		var formData = new FormData();
+
+		formData.append("email", this.inputNode1.value);
+		formData.append("password", this.inputNode2.value);
+
+		fetch("http://localhost:3001/api/tokens", {
+			method: 'POST', 
+			body: formData
+		}).then(res => res.json()).then(res => console.log(res.jwt))		
+			.then(() => this.props.history.push('/'))
+			.catch(function(error) {console.log('There is an error: ', error.message)});
+	}
+
 	render() {
 		return (
 			<div>
 			<h2>Login:</h2>
-			<form>
-	          <label htmlFor="email">Email: </label>
-	          <br />
-	          <input
-	            name="email"
-	            id="email"
-	            type="email"
-	          />
-	          <br /><br />
-	          <label htmlFor="password">Password:</label>
-	          <br />
-	          <input
-	            name="password"
-	            id="password"
-	            type="password"
-	          />
-          </form>
-          </div>
+				<form onSubmit={this.handleSubmit}>
+		          <label htmlFor="email">Email: </label>
+		          <br />
+		          <input
+		            name="email"
+		            id="email"
+		            type="email"
+		            ref={node => {this.inputNode1 = node}}
+		          />
+		          <br /><br />
+		          <label htmlFor="password">Password:</label>
+		          <br />
+		          <input
+		            name="password"
+		            id="password"
+		            type="password"
+		            ref={node => {this.inputNode2 = node}}
+		          />
+		          <br />
+		          <button type="submit">Log In</button>
+	          	</form>
+          	</div>
 		)
 	}
 }
