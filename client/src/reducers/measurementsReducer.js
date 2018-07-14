27c841d0fdf63e26954002c9ab3d1f01a0b1620c
debjohnson <debjohnson_11@yahoo.com>
2018-07-13 20:39:46 -0500

    Refactored measurementsReducer to allow for nested state

--- client/src/reducers/measurementsReducer.js
+++ client/src/reducers/measurementsReducer.js
@@ -1,9 +1,10 @@
-export default (state = [], action) => {
+export default (state = {}, action) => {
 	switch (action.type) {
 		case 'MEASUREMENTS_FETCH_DATA_SUCCESS':
 			return action.measurements;
 		case 'ADD_MEASUREMENT_SUCCESS':
-			return state.concat(action.measurement)
+			console.log(action.payload);
+			return {...state, measurements: {...state.measurements, [action.payload.userId]: action.payload } };
 		default:
 			return state;
 	}
