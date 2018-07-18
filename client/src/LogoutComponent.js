56fc8e9bebbdf471f400b5bba834836fe2488317
debjohnson <debjohnson_11@yahoo.com>
2018-07-17 19:21:22 -0500

    Edited Logout/NavBar for user logout

--- client/src/LogoutComponent.js
+++ client/src/LogoutComponent.js
@@ -2,7 +2,11 @@ import React, { Component } from 'react';
 
 class LogoutComponent extends Component {
 
-
+	componentWillMount() {
+		window.localStorage.clear('jwt');
+		alert('You are now Logged Out');
+    	this.props.history.push(`/login`);
+	}
 
 	render() {
 		return (
--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 //import './Medication.css';
 
@@ -11,38 +11,17 @@ const link = {
 	color: 'white',
 }
 
-class Navbar extends Component {
+const Navbar = () => 
+	<div className="nav">
+		<br />
+		<NavLink to='/' exact style={link}>Home</NavLink>
 
-	constructor(props){
-	    super(props);
-	    this.state = {};  
-	    this.handleLogout = this.handleLogout.bind(this); 
-	}
+		<NavLink to='/login' exact style={link}>Login</NavLink>
 
+		<NavLink to='/signup' exact style={link}>Sign Up</NavLink>
 
-	handleLogout = event => {
-		event.preventDefault();
-		console.log('Got here!');
-		window.localStorage.clear('jwt');
-		this.history.push(`/logout`);
-	}
+		<NavLink to='/logout' exact style={link}>Log Out</NavLink>
 
-	render() {
-
-		return (
-			<div className="nav">
-				<br />
-				<NavLink to='/' exact style={link}>Home</NavLink>
-
-				<NavLink to='/login' exact style={link}>Login</NavLink>
-
-				<NavLink to='/signup' exact style={link}>Sign Up</NavLink>
-
-				<NavLink to='/logout' exact style={link} onClick={this.handleLogout} >Log Out</NavLink>
-
-			</div>
-		)
-	}
-} 
+	</div>
 
 export default Navbar;
