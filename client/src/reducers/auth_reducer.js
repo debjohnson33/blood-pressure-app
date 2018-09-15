9e31b85b31185106051ab83e29969838f0fbb679
debjohnson33 <debjohnson_11@yahoo.com>
2018-09-15 17:01:26 -0500

    Added auth reducer with switch/case for actions

---
+++ client/src/reducers/auth_reducer.js
@@ -0,0 +1,49 @@
+export default (state = {
+    authenticating: false,
+    authenticated: false,
+    errors: [],
+    user: {},
+    current_user: ""
+}, action) => {
+    switch (action.type) {
+        case 'AUTHENTICATE_USER':
+            return {
+                ...state,
+                authenticating: true,
+                authenticated: false,
+                token: action.tokenauthenticate
+            }
+        case 'USER_AUTHENTICATED':
+            return {
+                ...state,
+                authenticated: true,
+                authenticating: false,
+                user: action.user,
+                current_user: action.user
+            }
+        case 'AUTHENTICATION_FAILURE':
+            return {
+                token: null,
+                email: "",
+                authenticating: false,
+                authenticated: false,
+                errors: action.error
+            }
+        case 'LOGGEDIN':
+            return {
+                ...state,
+                user: action.user,
+                authenticated: true
+            }
+        case 'LOGGEDOUT':
+            return {
+                ...state,
+                token: null,
+                authenticated: false,
+                authenticating: false,
+                user: {}
+            }
+    default:
+        return state
+    }
+}
