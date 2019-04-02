174bcd43ed61ccb462d19e2adb8dc921962942b4
debjohnson33 <debjohnson_11@yahoo.com>
2019-04-02 17:10:40 -0500

    Edited signup/added find method

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -14,10 +14,8 @@ class Api::UsersController < ApplicationController
 
 	def signup
 		@user = User.new(user_params)
-		if @user.save
-			token = Auth.create_token({id: @user.id, email: @user.email})
-			returned_user = Auth.decode_token(token)
-			render json: {id: @user.id, email: @user.email}, status: 201
+		if @user.valid? && @user.save
+			render json: @user, status: 201
 		else
 			render_errors_in_json
 		end
@@ -42,6 +40,16 @@ class Api::UsersController < ApplicationController
 			render status: 204
 		else
 			render json: {message: "Unable to process your request"}, status: 400
+		end
+	end
+
+	def find
+		@user = User.find_by(email: params[:user][:email])
+		if @user
+			render json: @user
+		else
+			@errors = @user.errors.full_messages
+			render json: @errors
 		end
 	end
 
