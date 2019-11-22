413a1027ae4f51fdd8fd01f9d67faa381d87a430
debjohnson33 <debjohnson_11@yahoo.com>
2019-11-21 22:10:16 -0600

    Implements resetGoalsForm action

--- client/src/actions/goals.js
+++ client/src/actions/goals.js
@@ -1,6 +1,6 @@
 import fetch from 'isomorphic-fetch';
 
-//import { resetGoalsForm } from './goalsForm';
+import { resetGoalsForm } from './goalsForm';
 
 const API_URL = "http://localhost:3001/api";
 
@@ -53,7 +53,7 @@ export const createGoal = (user_id, goal) => {
 			.then(response => response.json())
 			.then(goal => {
 				dispatch(addGoals(goal))
-			//	dispatch(resetGoalsForm())
+				dispatch(resetGoalsForm())
 			})
 			.catch(error => console.log(error))
 	}
