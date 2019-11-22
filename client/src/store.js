c8dcf57671a44f7497b208f692f652378541065d
debjohnson33 <debjohnson_11@yahoo.com>
2019-11-21 20:08:01 -0600

    Adds goalsReducer to store

--- client/src/store.js
+++ client/src/store.js
@@ -4,13 +4,15 @@ import thunk from 'redux-thunk';
 import measurementsReducer from './reducers/measurementsReducer';
 import measurementsFormData from './reducers/measurementsFormData';
 import auth_reducer from './reducers/auth_reducer';
+import goalsReducer from './reducers/goalsReducer';
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const reducers = combineReducers({
 	auth: auth_reducer,
 	measurements: measurementsReducer,
-	measurementsFormData
+	measurementsFormData,
+	goals: goalsReducer
 })
 
 const middleware = [thunk];
