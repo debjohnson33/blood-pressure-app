b3e1925044bbba4e7993b0cebd3a2c06ebca1744
debjohnson33 <debjohnson_11@yahoo.com>
2019-11-21 21:55:12 -0600

    Adds withRouter/changes GoalsPage Component

--- client/src/GoalsPage.js
+++ client/src/GoalsPage.js
@@ -1,17 +1,46 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 
-const GoalsPage = ({goals}) => {
-	return(
-		<div>
-			
-	        <h1 className="App-title">Goals:</h1>
-            <p>Systolic: {goals.systolic_bp}</p>
-            <p>Diastolic: {goals.diastolic_bp}</p>
-            <p>Frequency: {goals.frequency}</p>
-	        <br />
+class GoalsPage extends Component {
 
-		</div>
-	)
+    constructor(props) {
+		super(props);
+
+	}
+
+	handleSubmit = (e) => {
+		e.preventDefault();
+		this.props.history.push('/user_profile');
+    }
+    
+    handleToGoalsFormSubmit = (e) => {
+		e.preventDefault();
+		this.props.history.push(`/users/${this.props.user.id}/goals/new`)
+    }
+    
+    render () {
+        const { user, measurements, goals } = this.props;
+        let renderGoals;
+        if (goals) {
+            renderGoals = goals;
+        } else {
+            return <p>No goals set 	<button onClick={(e) => this.handleToGoalsFormSubmit(e) }>Click Here to Add Your Goals</button> <button onClick={(e) => this.handleSubmit(e) }>To User Profile Page</button>
+            </p>
+        }
+        return(
+            <div>
+                
+                <h1 className="App-title">Goals:</h1>
+                <p>Systolic: {goals.systolic_bp}</p>
+                <p>Diastolic: {goals.diastolic_bp}</p>
+                <p>Frequency: {goals.frequency}</p>
+                <br />
+
+            </div>
+        )
+
+    }
+        
 }
 
-export default GoalsPage;
+export default withRouter(GoalsPage);
