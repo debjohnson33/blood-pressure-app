7660f66296c3a4287e68b31c87f2f846f39ac7d9
Deb Johnson <friendswithdrj@gmail.com>
2020-04-08 17:03:00 -0500

    Adds editMeasurement/updateMeasurement action

--- client/src/actions/measurements.js
+++ client/src/actions/measurements.js
@@ -18,6 +18,13 @@ export const addMeasurements = measurement => {
 	}
 }
 
+export const editMeasurement = measurementId => {
+	return {
+		type: 'EDIT_MEASUREMENT_SUCCESS',
+		measurementId
+	}
+}
+
 export const destroyMeasurement = measurementId => {
 	return {
 		type: 'DELETE_MEASUREMENT_SUCCESS',
@@ -56,6 +63,31 @@ export const createMeasurement = (user_id, measurement) => {
 				dispatch(resetMeasurementsForm())
 			})
 			.catch(error => console.log(error))
+	}
+}
+
+export const updateMeasurement = (measurement) => {
+	const measurementId = measurement.id;
+	return dispatch => {
+		return fetch(`${API_URL}/measurements/${measurementId}`, {
+			user_id: measurement.user_id,
+			systolic_bp: measurement.systolic_bp,
+			diastolic_bp: measurement.systolic_bp,
+			pulse: measurement.pulse,
+			date_time: measurement.date_time,
+			notes: measurement.notes
+		},
+		{
+			method: 'PUT',
+			headers: {
+				"Authorization": `Bearer ${localStorage.token}`,
+			},
+		})
+		.then(response => response.json())
+		.then(() => {
+			dispatch.editMeasurement(measurementId)
+		})
+		.catch(error => console.log(error))
 	}
 }
 
