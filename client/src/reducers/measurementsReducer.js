c8ce617979ff723482b49bcb130263eb8a0d8d0e
Deb Johnson <friendswithdrj@gmail.com>
2020-04-08 17:28:07 -0500

    Adds update measurement to reducer

--- client/src/reducers/measurementsReducer.js
+++ client/src/reducers/measurementsReducer.js
@@ -7,6 +7,8 @@ export default (state = {}, action) => {
 			return {...state, measurements: {...state.measurements, [action.payload.userId]: action.payload } };
 		case 'DELETE_MEASUREMENT_SUCCESS':
 			return state.filter(measurement => measurement.id !== +action.measurementId);
+		case 'UPDATE_MEASUREMENT_SUCSSESS':
+			return {...state, measurement: action.measurement};
 		default:
 			return state;
 	}
