c0a83c56836836491b34df9ae2cb91291a2d655f
Deb Johnson <friendswithdrj@gmail.com>
2020-04-21 21:31:43 -0500

    Add editMode boolean prop for Measurement Form

--- client/src/UserProfile.js
+++ client/src/UserProfile.js
@@ -6,6 +6,12 @@ import MeasurementsForm from './MeasurementsForm';
 
 class UserProfile extends Component {
 
+    constructor(props) {
+        super(props);
+        const editMode = false;
+        this.state = editMode;
+    }
+
     handleSubmit = (e) => {
         e.preventDefault();
         this.props.history.push(`users/${this.props.user.id}/measurements`)
@@ -18,7 +24,7 @@ class UserProfile extends Component {
                 <h2>{this.props.user.email}</h2>
                 <p>{this.props.user.id}</p>
                 <button onClick={(e) => this.handleSubmit(e) }>To Measurements Page</button>
-                <MeasurementsForm user={this.props.user} />
+                <MeasurementsForm user={this.props.user} editMode={this.state.editMode}/>
             </div>
         )
     }
